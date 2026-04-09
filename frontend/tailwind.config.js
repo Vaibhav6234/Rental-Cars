af692cf9cb89cd003586dc6c0162ac97ca316b86
Vaibhav6234 <Vaibhav6234>
2026-04-09 13:40:17 +0530

    fix tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,1 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+// Tailwind v4 does not use this config file
